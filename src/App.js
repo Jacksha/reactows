72602af66e85e87f7a5a86d84a1c9da3f7dc80d2
Dario Jakovljević <jakovljevic.dario@gmail.com>
2024-06-13 23:27:58 +0200

    task-solved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Pozdrav from './Pozdrav';
 
 function App() {
   return (
-    <div className="App">
+    <><div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Dobrodošli na moj prvi React projekt!</h1>
+        <p>Ovo je moja prva aplikacija u Reactu.</p>
+        <Pozdrav ime = "Dario"></Pozdrav>
       </header>
-    </div>
+    </div></>
   );
 }
+
 
 export default App;
---
+++ src/Pozdrav.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Pozdrav = ( props ) => {
+     return <h2>Pozdrav, {props.ime}</h2>;
+}
+
+export default Pozdrav;
